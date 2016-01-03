876ce643da54cf4d576bedc9d3de90d76ff15bad
RedInquisitive <aj.walter@aol.com>
2016-01-02 23:12:17 -0600

    Clear error on update.

--- src/windows/Scanner.java
+++ src/windows/Scanner.java
@@ -21,12 +21,18 @@ import util.Keyboard;
 @SuppressWarnings("serial")
 public class Scanner extends JFrame{	
 	
+	private volatile long counter = 0L;	
+	private volatile boolean needsUpdate = false;
+	
 	private String roomName;
 	private JLabel title;
+	private String normalTitle = "?";
 	private JLabel subtext;
+	private String normalSubtext = "<html><center>Please use the barcode scanner and your Student ID to sign in and out of this room.</center></html>";
 	public Scanner(String roomName, String hash) {
 		super("Scanner Utility");
         this.roomName = roomName.substring(0, 1).toUpperCase() + roomName.substring(1);
+        this.normalTitle = "<html><center>" + this.roomName + " sign in/out station</center></html>";
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		int xSize = ((int) tk.getScreenSize().getWidth());
 		int ySize = ((int) tk.getScreenSize().getHeight());
@@ -38,6 +44,23 @@ public class Scanner extends JFrame{
         this.pack();
         this.setVisible(true);
         this.addKeyListener(new Keyboard(this, roomName, hash));
+        this.counter = System.currentTimeMillis();
+        new Thread() {
+        	public void run() {
+        		while(true) {
+        			if(counter < System.currentTimeMillis() - 3000 && needsUpdate) {
+        				needsUpdate = false;
+        				title.setText(normalTitle);
+        				subtext.setText(normalSubtext);
+        			}
+        			try {
+						Thread.sleep(100L);
+					} catch (InterruptedException e) {
+						continue;
+					}
+        		}
+        	}
+        }.start();
 	}
 	
 	public void addComponentsToPane(Container p) {
@@ -59,7 +82,7 @@ public class Scanner extends JFrame{
 	    c.gridy = 0;
 	    p.add(label, c);
 	    
-		title = new JLabel("<html><center>" + roomName + " sign in/out station</center></html>",SwingConstants.CENTER);
+		title = new JLabel(normalTitle,SwingConstants.CENTER);
 		Font labelFont = title.getFont();
 		title.setFont(new Font(labelFont.getName(), Font.PLAIN, 86));
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -68,7 +91,7 @@ public class Scanner extends JFrame{
 	    c.gridy = 0;
 	    p.add(title, c);
 	    
-		subtext = new JLabel("<html><center>Please use the barcode scanner and your Student ID to sign in and out of this room.</center></html>",SwingConstants.CENTER);
+		subtext = new JLabel(normalSubtext,SwingConstants.CENTER);
 		labelFont = subtext.getFont();
 		subtext.setFont(new Font(labelFont.getName(), Font.PLAIN, 40));
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -108,9 +131,13 @@ public class Scanner extends JFrame{
 	
 	public void setHeaderText(String text) {
 		title.setText("<html><center>" + text + "</center></html>");
+		counter = System.currentTimeMillis();
+		needsUpdate = true;
 	}
 	
 	public void setSubtitleText(String text) {
 		subtext.setText("<html><center>" + text + "</center></html>");
+		counter = System.currentTimeMillis();
+		needsUpdate = true;
 	}
 }
